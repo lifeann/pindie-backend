b9364d004d592c838fa292aec6392de94bdb25ed
lifeann <149606573+lifeann@users.noreply.github.com>
2024-04-29 20:49:33 +0400

    часть 1

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
 const gamesRouter = require("./routes/games");
 const usersRouter = require("./routes/users");
 const categoriesRouter = require("./routes/categories");
+const connectToDatabase = require("./database/connect");
+const cors = require("./middlewares/cors");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+connectToDatabase();
 
 app.use(
+  cors,
   bodyParser.json(),
   express.static(path.join(__dirname, "public")),
   gamesRouter,
---
+++ controllers/users.js
@@ -0,0 +1,8 @@
+const sendAllUsers = (req, res) => {
+    
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(req.usersArray));
+  };
+  
+  module.exports = sendAllUsers;
+  
