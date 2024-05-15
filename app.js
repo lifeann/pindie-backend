9b1ce193b98c7fec278eccc81ce7330bcb334d23
lifeann <149606573+lifeann@users.noreply.github.com>
2024-05-15 10:14:01 +0400

    cookie

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require("path");
 const connectToDatabase = require("./database/connect");
 const cors = require("./middlewares/cors");
 const apiRouter = require("./routes/api");
+const cookieParser = require("cookie-parser");
+const pagesRouter = require("./routes/pages");
 
 const app = express();
 const PORT = process.env.PORT;
@@ -11,7 +13,9 @@ connectToDatabase();
 
 app.use(
   cors,
+  cookieParser(),
   bodyParser.json(),
+  pagesRouter,
   apiRouter,
   express.static(path.join(__dirname, "public")),
   );
---
+++ routes/pages.js
@@ -0,0 +1,7 @@
+const pagesRouter = require("express").Router(); 
+
+const { sendIndex } = require("../controllers/auth.js");
+
+pagesRouter.get("/", sendIndex); 
+
+module.exports = pagesRouter;
